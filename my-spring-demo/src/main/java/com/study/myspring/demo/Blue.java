27fe5c9fdb6f956a75a07860d41c71c339ad5d2b
love550185890 <lsf_320@126.com>
2019-10-10 00:06:41 +0800

    spring5.1.11源码构建

---
+++ my-spring-demo/src/main/java/com/study/myspring/demo/Blue.java
@@ -0,0 +1,17 @@
+package com.study.myspring.demo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description
+ * @Author
+ * @Date 2019年10月9日09:27:17
+ */
+@Component("blue")
+public class Blue {
+
+	public Blue() {
+		System.out.println("Blue...init");
+	}
+
+}
---
+++ my-spring-demo/src/main/java/com/study/myspring/demo/TestMySpring.java
@@ -0,0 +1,18 @@
+package com.study.myspring.demo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @Description
+ * @Author
+ * @Date 2019年10月9日09:25:28
+ */
+@ComponentScan("com.study.myspring.demo")
+public class TestMySpring {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestMySpring.class);
+		Blue blue = (Blue) context.getBean("blue");
+	}
+}
